c2a192b8047628d0ea71f9034fbd97791a859e86
Erik Brinkman <erik.brinkman@gmail.com>
2021-01-24 02:35:18 -0500

    add optional tuple typing

--- mod.ts
+++ mod.ts
@@ -1,23 +1,36 @@
 import { solveQP } from "./lib/quadprog.js";
 
-export interface Solution {
-  solution: number[];
-  lagrangian: number[];
-  unconstrained: number[];
+type _TupleOf<T, N extends number, R extends unknown[]> = R["length"] extends N
+  ? R
+  : _TupleOf<T, N, [T, ...R]>;
+type Tuple<T, N extends number> = N extends N
+  ? number extends N ? T[] : _TupleOf<T, N, []>
+  : never;
+export type Vector<N extends number = number> = Tuple<number, N>;
+export type Matrix<N extends number = number, M extends number = number> =
+  Tuple<Vector<M>, N>;
+
+export interface Solution<N extends number> {
+  solution: Vector<N>;
+  lagrangian: Vector<N>;
+  unconstrained: Vector<N>;
   iterations: number;
   inactive: number;
   active: number[];
   value: number;
 }
 
-export default function (
-  qmat: number[][],
-  cvec: number[],
-  amat: number[][],
-  bvec: number[],
+export default function solve<
+  N extends number = number,
+  C extends number = number,
+>(
+  qmat: Matrix<N, N>,
+  cvec: Vector<N>,
+  amat: Matrix<C, N>,
+  bvec: Vector<C>,
   meq = 0,
   factorized = false,
-) {
+): Solution<N> {
   const Dmat = [[0]].concat(qmat.map((row) => [0].concat(row)));
   const dvec = [0].concat(cvec.map((v) => -v));
   const Amat = [[0]].concat(
@@ -56,9 +69,9 @@ export default function (
     const [, iterations, inactive] = iters;
 
     return {
-      solution,
-      lagrangian,
-      unconstrained,
+      solution: solution as Vector<N>,
+      lagrangian: lagrangian as Vector<N>,
+      unconstrained: unconstrained as Vector<N>,
       iterations,
       inactive,
       active,
